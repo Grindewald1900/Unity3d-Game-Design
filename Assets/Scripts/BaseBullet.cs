658a0a9eddbf98a8d0a76f663bcc472e867a5bde
Yee <17788108425@163.com>
2020-09-21 23:05:43 -0400

    Player model

---
+++ Assets/Scripts/BaseBullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseBullet : MonoBehaviour
+{
+    private float _timeAlive = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        _timeAlive += Time.deltaTime;
+        if (_timeAlive > 3)
+        {
+            gameObject.SetActive(false);
+            _timeAlive = 0f;
+        }
+    }
+}
---
+++ Assets/Scripts/BaseGunControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseGunControl : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newBullet = ObjectPool.SharedInstance.GetPoolObjects();
+            newBullet.transform.position = transform.position + transform.forward;
+            // newBullet.GetComponent<Rigidbody>().velocity = new Vector3(-1,0,0);
+            newBullet.SetActive(true);
+            audioSource.Play();
+
+        }
+    }
+}
