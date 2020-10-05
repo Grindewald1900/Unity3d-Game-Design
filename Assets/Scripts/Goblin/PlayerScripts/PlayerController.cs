0d184a0c6bfc75671062b54d9198d36673afe369
Yee <17788108425@163.com>
2020-10-05 14:19:22 -0400

    Map and humanoid model

--- Assets/Scripts/Goblin/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/Goblin/PlayerScripts/PlayerController.cs
@@ -6,9 +6,13 @@ namespace Goblin.PlayerScripts
     {
         public CharacterController thirdPersonController;
         public Transform mainCamera;
+        public Animator playerAnimator;
         private float _speed;
         private float _turnSmoothTime;
         private float _currentVelocity;
+
+        private static readonly int Walk = Animator.StringToHash("walk");
+
         // Start is called before the first frame update
         private void Start()
         {
@@ -22,7 +26,10 @@ namespace Goblin.PlayerScripts
             var horizontal = Input.GetAxis("Horizontal");
             var vertical = Input.GetAxis("Vertical");
             var direction = new Vector3(horizontal, 0, vertical).normalized;
-            if (direction.magnitude < 0.1f) return;
+            if (direction.magnitude < 0.1f) {
+                playerAnimator.SetBool(Walk,false);
+                return;
+            }
             
             // Atan2 :Returns the angle in radians whose Tan is y/x.
             // Rad2Deg: Rad2Deg = 57.29578f, Radians-to-degrees conversion constant 
@@ -31,6 +38,7 @@ namespace Goblin.PlayerScripts
             var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, _turnSmoothTime);
             var moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
+            playerAnimator.SetBool(Walk,true);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             thirdPersonController.Move(moveDirection.normalized * (_speed * Time.deltaTime));
         }
