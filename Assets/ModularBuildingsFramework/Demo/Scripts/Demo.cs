6575c7ee1019e7af0ddd00c6ac639b8cb18c8982
Yee <17788108425@163.com>
2020-10-08 23:30:12 -0400

    import some environment models

---
+++ Assets/ModularBuildingsFramework/Demo/Scripts/Demo.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+using UnityEngine;
+
+namespace ModularBuildingsFramework.Demo
+{
+	public class Demo : MonoBehaviour
+	{
+        /// ======================================================================
+
+        [SerializeField] private List<Object> builders;
+
+        private int _builderIndex;
+        private float _draftLength;
+        private float _draftHeight;
+        private float _draftDepth;
+        private bool _isRunDecorators;
+
+        private Camera sceneCamera;
+        private Transform target;
+        private bool isDraftDirty;
+                
+        /// ======================================================================
+
+        public int builderIndex
+        {
+            get { return _builderIndex; }
+            set
+            {
+                _builderIndex = value;
+                isDraftDirty = true;
+            }
+        }
+        public float draftLength
+        {
+            get { return _draftLength; }
+            set
+            {
+                _draftLength = value;
+                isDraftDirty = true;
+            }
+        }
+        public float draftHeight
+        {
+            get { return _draftHeight; }
+            set
+            {
+                _draftHeight = value;
+                isDraftDirty = true;
+            }
+        }
+        public float draftDepth
+        {
+            get { return _draftDepth; }
+            set
+            {
+                _draftDepth = value;
+                isDraftDirty = true;
+            }
+        }
+        public bool isRunDecorators
+        {
+            get { return _isRunDecorators; }
+            set
+            {
+                _isRunDecorators = value;
+                isDraftDirty = true;
+            }
+        }
+
+        /// ======================================================================
+
+        private void Awake()
+        {
+            sceneCamera = GameObject.Find("SCENE/MainCamera").GetComponent<Camera>();
+            target = new GameObject("TARGET").transform;
+
+            draftLength = 20f;
+            draftHeight = 20f;
+            draftDepth = 20f;
+
+            isRunDecorators = true;
+            sceneCamera.enabled = false;
+        }
+        private IEnumerator Start()
+        {
+            StartCoroutine(DoRebuild());
+
+            yield return new WaitForSeconds(1f);
+            sceneCamera.enabled = true;
+        }
+
+        public void RequestRebuild()
+        {
+            isDraftDirty = true;
+        }
+        private IEnumerator DoRebuild()
+        {
+            while (true)
+            {
+                yield return new WaitForEndOfFrame();
+
+                if (isDraftDirty)
+                {
+                    Utils.DeleteChilds(target);
+                    UpdateTargetPosition();
+
+                    var draft = CreateDraft();
+                    var builder = GetBuilder();
+                    builder.Build(draft);
+
+                    if (isRunDecorators)
+                        Utils.RunDecorators(target);
+
+                    isDraftDirty = false;
+                }
+            }
+        }
+
+        private void UpdateTargetPosition()
+        {
+            target.position = Vector3.zero
+                - target.right * draftLength * 0.5f
+                - target.forward * draftDepth * 0.5f;
+        }
+        private BoxDraft CreateDraft()
+        {
+            var result = BoxDraft.Create();
+            result.Parse(target);
+
+            result.length = draftLength;
+            result.height = draftHeight;
+            result.depth = draftDepth;
+
+            return result;
+        }
+
+        public List<Object> GetBuilders()
+        {
+            return builders;
+        }
+        private IBoxBuilder GetBuilder()
+        {
+            return builders[builderIndex] as IBoxBuilder;
+        }
+
+        /// ======================================================================
+
+        private Demo()
+        {
+            builders = new List<Object>();
+        }
+
+        /// ======================================================================
+    }
+}
---
+++ Assets/ModularBuildingsFramework/Demo/Scripts/DemoUI.cs
@@ -0,0 +1,134 @@
+﻿ using System;
+using System.Collections;
+using System.Collections.Generic;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+using UnityEngine;
+using UnityEngine.UI;
+using ModularBuildingsFramework;
+
+namespace ModularBuildingsFramework.Demo
+{
+	public class DemoUI : MonoBehaviour
+	{
+        /// ======================================================================
+
+        [SerializeField] private Dropdown buildersDropdown = default(Dropdown);
+
+        [Space]
+        [SerializeField] private Text lengthLabel = default(Text);
+        [SerializeField] private Slider lengthSlider = default(Slider);
+
+        [Space]
+        [SerializeField] private Text heightLabel = default(Text);
+        [SerializeField] private Slider heightSlider = default(Slider);
+
+        [Space]
+        [SerializeField] private Text depthLabel = default(Text);
+        [SerializeField] private Slider depthSlider = default(Slider);
+
+        [Space]
+        [SerializeField] private Toggle decoratorsToggle = default(Toggle);
+        [SerializeField] private Button generateButton = default(Button);
+
+        private Demo demo;
+
+        /// ======================================================================
+
+        private void Awake()
+        {
+            demo = FindObjectOfType<Demo>();
+
+            buildersDropdown.ClearOptions();
+
+            foreach (var builder in demo.GetBuilders())
+            {
+                var option = new Dropdown.OptionData(builder.name);
+                buildersDropdown.options.Add(option);
+            }
+
+            buildersDropdown.value = 1;
+            buildersDropdown.value = 0;
+
+            lengthSlider.value = 20f;
+            heightSlider.value = 20f;
+            depthSlider.value = 20f;
+            
+            decoratorsToggle.isOn = true;
+
+            RedrawLength();
+            RedrawHeight();
+            RedrawDepth();
+        }
+        private void OnEnable()
+        {
+            buildersDropdown.onValueChanged.AddListener(OnBuildersDropdownValueChanged);
+
+            lengthSlider.onValueChanged.AddListener(OnLengthSliderValueChanged);
+            heightSlider.onValueChanged.AddListener(OnHeightSliderValueChanged);
+            depthSlider.onValueChanged.AddListener(OnDepthSliderValueChanged);
+
+            decoratorsToggle.onValueChanged.AddListener(OnDecoratorsToggleChanged);
+            generateButton.onClick.AddListener(OnGenerateButtonClicked);
+        }
+        private void OnDisable()
+        {
+            buildersDropdown.onValueChanged.RemoveListener(OnBuildersDropdownValueChanged);
+
+            lengthSlider.onValueChanged.RemoveListener(OnLengthSliderValueChanged);
+            heightSlider.onValueChanged.RemoveListener(OnHeightSliderValueChanged);
+            depthSlider.onValueChanged.RemoveListener(OnDepthSliderValueChanged);
+
+            decoratorsToggle.onValueChanged.RemoveListener(OnDecoratorsToggleChanged);
+            generateButton.onClick.RemoveListener(OnGenerateButtonClicked);
+        }
+
+        private void RedrawLength()
+        {
+            lengthLabel.text = "Length: " + Mathf.RoundToInt(lengthSlider.value);
+        }
+        private void RedrawHeight()
+        {
+            heightLabel.text = "Height: " + Mathf.RoundToInt(heightSlider.value);
+        }
+        private void RedrawDepth()
+        {
+            depthLabel.text = "Depth: " + Mathf.RoundToInt(depthSlider.value);
+        }
+
+        /// ======================================================================
+
+
+        private void OnBuildersDropdownValueChanged(int value)
+        {
+            demo.builderIndex = value;
+        }
+
+        private void OnLengthSliderValueChanged(float value)
+        {
+            demo.draftLength = value;
+            RedrawLength();
+        }
+        private void OnHeightSliderValueChanged(float value)
+        {
+            demo.draftHeight = value;
+            RedrawHeight();
+        }
+        private void OnDepthSliderValueChanged(float value)
+        {
+            demo.draftDepth = value;
+            RedrawDepth();
+        }
+
+        private void OnDecoratorsToggleChanged(bool value)
+        {
+            demo.isRunDecorators = value;
+        }
+        private void OnGenerateButtonClicked()
+        {
+            demo.RequestRebuild();
+        }
+
+        /// ======================================================================
+    }
+}
